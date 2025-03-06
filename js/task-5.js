1538164684c0f541d7d0269179bf70719c29fa45
Andriy Romanov <mgm.agro04@gmail.com>
2025-03-06 20:36:30 +0200

    task-5js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+"use strict";
+//Function to generate a random color.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorButton = document.querySelector(`.change-color`); //Get the button.
+const colorText = document.querySelector(`.color`); //Get the <span> once so as not to call querySelector in the function.
+//Add a click event.
+changeColorButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color; //Change the background.
+  colorText.textContent = color; //Update the color value.
+});
